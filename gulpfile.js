bc19ea5e8528f4506d3280d7581afb88d0756b3f
LEv145 <you@example.com>
2022-08-31 18:13:17 +0200

    v 0.1
    Настроена сборка и файловая структура

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,101 @@
-let gulp = require("gulp");
-let concat = require("gulp-concat");
-let concatCss = require("gulp-concat-css");
-let browserSync = require("browser-sync");
-let Filter = require("gulp-filter");
-let sass = require('gulp-sass')(require('sass'));
+"use strict";
+
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync");
+const dartSass = require("sass");
+const gulpSass = require("gulp-sass");
+const BemBuilder = require("gulp-bem-bundle-builder");
+const bundler = require("gulp-bem-bundler-fs");
+const debug = require("gulp-debug");
+const bemxjst = require("gulp-bem-xjst");
+// const through2Filter = require("through2-filter");
 
 
-const basePath = "src/";
-let BrowserSync = browserSync.create();
+const sourcePath = "src";
+const blocksPath = `${sourcePath}/blocks`
+const bundlesPath = `${sourcePath}/bundles`
+const buildPath = "build"
+
+const sass = gulpSass(dartSass);
+const bemhtml = bemxjst.bemhtml;
+const toHtml = bemxjst.toHtml;
+const BrowserSync = browserSync.create();
+const bemBuilder = BemBuilder({
+    levels: [
+        "node_modules/bem-core/common.blocks",
+        "node_modules/bem-core/desktop.blocks",
+        "node_modules/bem-components/common.blocks",
+        "node_modules/bem-components/desktop.blocks",
+        "node_modules/bem-components/design/common.blocks",
+        "node_modules/bem-components/design/desktop.blocks",
+        blocksPath,
+    ],
+    techMap: {
+        bemhtml: ["bemhtml.js"],
+        js: ["js"],
+        css: ["sass", "css"],
+    },
+});
 
 
-gulp.task("build-css", function () {
-    let sassus_filter = Filter('**/*.sass', {restore: true})
-
+function build() {
     return (
-        gulp
-        .src(`${basePath}blocks/**/*.{sass,css}`)
-        // If Sass
-        .pipe(sassus_filter)
-        .pipe(sass().on("error", sass.logError))
-        .pipe(sassus_filter.restore)
-
-        .pipe(concatCss("style.css", {commonBase: basePath, rebaseUrls: false}))
-        .pipe(gulp.dest(basePath))
+        bundler(`${bundlesPath}/*`)
+        .pipe(bemBuilder({
+            css: bundle =>
+                (
+                    bundle.src("css")
+                    .pipe(sass().on("error", sass.logError))
+                    .pipe(concat(bundle.name + ".min.css"))
+                ),
+            js: bundle =>
+                (
+                    bundle.src("js")
+                    .pipe(concat(bundle.name + ".min.js"))
+                ),
+            tmpls: bundle =>
+                (
+                    bundle.src("bemhtml")
+                    .pipe(concat("any.bemhtml.js"))
+                    .pipe(bemhtml({elemJsInstances: true}))
+                    .pipe(concat(bundle.name + ".bemhtml.js"))
+                ),
+            html: bundle => {
+                const bemhtmlApply = () => toHtml(bundle.target("tmpls"));
+                return (
+                    gulp
+                    .src(bundle.dirname + "/*.bemjson.js")
+                    .pipe(bemhtmlApply())
+                );
+            }
+       }))
+       .on("error", console.error)
+       .pipe(debug())
+       .pipe(gulp.dest(buildPath))
     );
-});
+};
 
-gulp.task("build-js", function () {
-    return (
-        gulp
-        .src(`${basePath}blocks/**/*.js`)
-        .pipe(concat("script.js"))
-        .pipe(gulp.dest(basePath))
+function dev() { 
+    BrowserSync.init({server: {baseDir: buildPath}, open: false});
+    // Blocks
+    gulp.watch(
+        `${blocksPath}/**/*.{sass,css,js}`, 
+        gulp.series(build, _reloadBrowserSync),
     );
-});
+    // Bundles
+    gulp.watch(
+        `${bundlesPath}/*/*.{bemjson.js}`, 
+        _reloadBrowserSync,
+    );
+}
 
-gulp.task("browser-sync", function() {
-    BrowserSync.init({server: {baseDir: basePath}});
-});
-
-gulp.task("browser-sync-reload", function(done) {
+function _reloadBrowserSync(next) {
     BrowserSync.reload();
-    done();
-});
+    next();
+}
 
-gulp.task("dev", gulp.series(function() {
-    BrowserSync.init({server: {baseDir: basePath}, open: false});
-    gulp.watch(`${basePath}blocks/**/*.js`, gulp.series("build-js", "browser-sync-reload"));
-    gulp.watch(`${basePath}blocks/**/*.{sass,css}`, gulp.series("build-css", "browser-sync-reload"));
-    gulp.watch(`${basePath}*.html`, gulp.series("browser-sync-reload"));
-}));
 
-gulp.task("default", gulp.series("dev"));
+exports.build = build;
+exports.dev = dev;
+exports.default = dev;
