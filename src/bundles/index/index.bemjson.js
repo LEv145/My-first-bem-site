9dafe6487d072bef661dd0469259fd4c2de07376
LEv145 <you@example.com>
2022-08-31 18:14:14 +0200

    v 0.1
    Дополнение

--- src/bundles/index/index.bemjson.js
+++ src/bundles/index/index.bemjson.js
@@ -1,10 +1,9 @@
 module.exports = {
-    block: 'page',
-    title: 'Title of the page',
+    block: "page",
+    title: "Title of the page",
     head: [
-        { elem: 'meta', attrs: { name: 'description', content: '' } },
-        { elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
-        { elem: 'css', url: 'index.min.css' }
+        {elem: "meta", attrs: {name: "viewport", content: "width=device-width, initial-scale=1"}},
+        {elem: "css", url: "index.min.css"}
     ],
-    scripts: [{ elem: 'js', url: 'index.min.js' }],
+    scripts: [{elem: "js", url: "index.min.js"}],
 };
